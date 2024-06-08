2c6deffaf6e461cede507f9f873e0fc067b975e9
Roggers Anguzu <141458053+roggersanguzu@users.noreply.github.com>
2024-06-08 17:30:01 +0300

    OOp_Session_One

---
+++ Object_and_Classes/Car.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Object_and_Classes;
+
+/**
+ *
+ * @author ubuntu
+ */
+public class Car {
+    String make="Chevrolet";
+    String model="Corvellet";
+    int year=2024;
+    String color="blue";
+    double price=50000.00;
+    
+    void drive(){
+        System.out.print("You Drive the car");
+    }
+    void brake(){
+        System.out.print("Stsep on the brakes");
+    }
+    
+}
---
+++ Object_and_Classes/Oop_2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Object_and_Classes;
+
+/**
+ *
+ * @author ubuntu
+ */
+public class Oop_2 {
+    public static void main(String[] args){
+        Car mycar=new Car();
+        System.out.println(mycar.model + " Is the car Model");
+        
+    }
+    
+}
